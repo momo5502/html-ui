9df9cc438bb0dd6aac4ecee5ad8e487be0d063f0
momo5502 <mauriceheumann@gmail.com>
2023-08-21 21:07:11 +0200

    Add better sample

--- sample/main.cpp
+++ sample/main.cpp
@@ -7,12 +7,65 @@ int APIENTRY WinMain(HINSTANCE, HINSTANCE, PSTR, int)
 	window.load_html(R"code(
 <!DOCTYPE html>
 <html>
-<body> 
 
-<h1>Heading 1</h1>
-<button onclick="window.external.testFunc('hello world', 1234)">Button</button>
+<head>
+    <style>
+        html {
+            height: 100%;
 
+            background: hsla(139, 70%, 75%, 1);
+            background: linear-gradient(90deg, hsla(139, 70%, 75%, 1) 0%, hsla(63, 90%, 76%, 1) 100%);
+            background: -moz-linear-gradient(90deg, hsla(139, 70%, 75%, 1) 0%, hsla(63, 90%, 76%, 1) 100%);
+            background: -webkit-linear-gradient(90deg, hsla(139, 70%, 75%, 1) 0%, hsla(63, 90%, 76%, 1) 100%);
+            filter: progid: DXImageTransform.Microsoft.gradient(startColorstr="#95ECB0", endColorstr="#F3F98A", GradientType=1);
+        }
+
+        * {
+            font-family: -apple-system, BlinkMacSystemFont, segoe ui, Roboto, Oxygen, Ubuntu, Cantarell, open sans, helvetica neue, sans-serif;
+        }
+
+        h1 {
+            color: rgb(57, 57, 57);
+        }
+
+        button {
+            display: inline-block;
+            outline: none;
+            cursor: pointer;
+            font-size: 14px;
+            line-height: 1;
+            border-radius: 500px;
+            transition-property: background-color, border-color, color, box-shadow, filter;
+            transition-duration: .3s;
+            border: 1px solid transparent;
+            letter-spacing: 2px;
+            min-width: 160px;
+            text-transform: uppercase;
+            white-space: normal;
+            font-weight: 700;
+            text-align: center;
+            padding: 16px 14px 18px;
+            color: rgb(57, 57, 57);
+            box-shadow: inset 0 0 0 2px rgb(57, 57, 57);
+            background-color: transparent;
+            height: 48px;
+        }
+
+        button:hover {
+            color: #fff;
+            background-color: rgb(57, 57, 57);
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <h1>HTML-UI</h1>
+        <br>
+        <button onclick="window.external.testFunc('hello world', 1234)">Button</button>
+    </center>
 </body>
+
 </html>
 )code");
 
